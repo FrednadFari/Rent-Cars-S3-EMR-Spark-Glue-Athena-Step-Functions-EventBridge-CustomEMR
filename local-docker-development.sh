4b9dca28f5a2b29d171ef71a1cb6fe42cae013c6
Fardis Foroughi <fredandfari@gmail.com>
2026-04-25 19:59:25 -0700

    new updata

---
+++ local-docker-development.sh
@@ -0,0 +1,14 @@
+$WORKSPACE_LOCATION = (Get-Location).Path
+$SCRIPT_FILE_NAME = "spark-agg2.py"
+
+docker run -it `
+-v ${HOME}\.aws:/home/glue_user/.aws `
+-v ${WORKSPACE_LOCATION}:/home/glue_user/workspace `
+-e AWS_PROFILE=default `
+-e DISABLE_SSL=true `
+--rm `
+-p 4040:4040 `
+-p 18080:18080 `
+--name glue_spark_submit `
+amazon/aws-glue-libs:glue_libs_4.0.0_image_01 `
+spark-submit /home/glue_user/workspace/$SCRIPT_FILE_NAME
